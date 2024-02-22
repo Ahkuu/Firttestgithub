3c89e3e196f150ef0848637034d40acf0c73a6b1
Ahkuu <Dylantwhite89@yahoo.com>
2024-02-22 17:51:59 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+//Just a comment with practice for a statement on Javascript
+console.log("Hello World");
+// A variable stores data temporarily. 
+let name = 'Dylan';
+console.log(name);
+
+//Cannot be a reserved key word
+//Should be a meaningful name
+//Cannot start with a number (1name)
+//Cannot contain a space or hyphen (-)
+//Are case-sensitive
+
+let firstName = "Dielawn";
+console.log(firstName);
